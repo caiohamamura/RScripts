6305f630623d430a5ec24051e84f340ceaf1c89b
chamamur <caiohamamura@gmail.com>
2013-02-07 14:59:18 -0200

    modified:   ndvi.r

--- ndvi.r
+++ ndvi.r
@@ -1,5 +1,8 @@
 require(raster)
+args <- commandArgs(trailingOnly = TRUE)
+print(args)
 
-img = stack('D:/Caio/RioClaro/rcteste_kuwahara.tif')
+setOptions('GTiff')
+img = stack(args[1])
 ndvi = round((((img[[1]]-img[[2]])/(img[[2]]+img[[1]]))+1)*(255/2))
-writeRaster(ndvi, 'D:/Caio/RioClaro/rcteste_ndvi_kuwahara.tif', datatype='INT1U')
+writeRaster(ndvi, args[2], datatype='INT1U')
